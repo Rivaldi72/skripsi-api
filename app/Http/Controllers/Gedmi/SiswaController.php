37eb488529bdd363b8fbf5fc63a27cd04756b0df
Rivaldi Aliando <rivdew@gmail.com>
2022-09-30 22:11:08 +0700

    Selesaikan Filter Siswa Gedmi

--- app/Http/Controllers/Gedmi/SiswaController.php
+++ app/Http/Controllers/Gedmi/SiswaController.php
@@ -8,8 +8,17 @@ use App\Models\Gedmi\SiswaModel;
 
 class SiswaController extends Controller
 {
-    public function siswa1(){
-        $dataSiswa = SiswaModel::all();
+    public function siswa1(Request $request){
+        // dd($request->all());
+        if ($request->filter == "VII") {
+            $dataSiswa = SiswaModel::where('kelas', "VII")->get();
+        } elseif ($request->filter == "VIII") {
+            $dataSiswa = SiswaModel::where('kelas', "VIII")->get();
+        } elseif ($request->filter == "IX") {
+            $dataSiswa = SiswaModel::where('kelas', "IX")->get();
+        } else {
+            $dataSiswa = SiswaModel::all();
+        }
         return view('content.pages.gedmi.siswa.indexsiswa', compact('dataSiswa'));
     }
     public function tambahSiswa(){
