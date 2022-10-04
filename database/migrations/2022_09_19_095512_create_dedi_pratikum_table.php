b37d5eede543a49fdf5cb75ecb048e0146aef550
unknown <dedi.thebleckmits@gmail.com>
2022-10-04 21:07:34 +0700

    tambah data

--- database/migrations/2022_09_19_095512_create_dedi_pratikum_table.php
+++ database/migrations/2022_09_19_095512_create_dedi_pratikum_table.php
@@ -27,33 +27,33 @@ class CreateDediPratikumTable extends Migration
                 'id_mapel' => 1,
                 'judul_pratikum' => 'Pewarnaan Sederhana' ,
                 'detail_pratikum' => 'Kompetensi Dasar : Mampu Melakukan Prosedur Pewarnaan
-Indikator : mampu memahami bentuk-bentuk bakteri, mampu melakukan prosedur pewarnaan
-Tujuan : untuk melihat bentuk bentuk bakteri
-Bahan : biakan dalam bouillon
-Regensia : fuchsin 0,5%
-Komposisi regensia : fuchsin 0,5 gr
--	Aquadest 100ml
- Alat : -- ose cincin
--	Rak tabung
--	Objek glass
--	Mikroskop
--	Lampu bunsen
-Cara kerja : - Hidupkan lampu Bunsen
--	Bakar ose cincin hingga merah membara, kemudian dinginkan
--	Ambil bahan biakan dalam bouillon 3-5 ose cincin
--	Kemudian letakkan di atas objek glass,kemudian di ratakan
--	Difiksasi sediaan sampai kering, lalu dinginkan
--	Tetesin dengan regensia, lalu tunggu selama 5 menit
--	Kemudian buang cairan regensia yang ada di objek glass lalu di alirkan dengan air mengalir sampai tidak ada regensia lagi
--	Tetesin minyak imersi oil di atas sediaan
--	Kemudian lihat dibawah mikroskop lensa 100x
+                Indikator : mampu memahami bentuk-bentuk bakteri, mampu melakukan prosedur pewarnaan
+                Tujuan : untuk melihat bentuk bentuk bakteri
+                Bahan : biakan dalam bouillon
+                Regensia : fuchsin 0,5%
+                Komposisi regensia : fuchsin 0,5 gr
+                -	Aquadest 100ml
+                Alat : -- ose cincin
+                -	Rak tabung
+                -	Objek glass
+                -	Mikroskop
+                -	Lampu bunsen
+                Cara kerja : - Hidupkan lampu Bunsen
+                -	Bakar ose cincin hingga merah membara, kemudian dinginkan
+                -	Ambil bahan biakan dalam bouillon 3-5 ose cincin
+                -	Kemudian letakkan di atas objek glass,kemudian di ratakan
+                -	Difiksasi sediaan sampai kering, lalu dinginkan
+                -	Tetesin dengan regensia, lalu tunggu selama 5 menit
+                -	Kemudian buang cairan regensia yang ada di objek glass lalu di alirkan dengan air mengalir sampai tidak ada regensia lagi
+                -	Tetesin minyak imersi oil di atas sediaan
+                -	Kemudian lihat dibawah mikroskop lensa 100x
 
-Hasil gambar di temukan :
-',
+                Hasil gambar di temukan :',
                 'gambar' => '',
             ],
 
         ];
+        DB::table('dedi_pratikum')->insert($defaulData);
     }
 
     /**
