fb9b3643544cd9fa385cfd6e754bc01c036a03c6
Rivaldi Aliando <rivdew@gmail.com>
2022-10-19 21:01:45 +0700

    Cek Password

--- app/Http/Controllers/Gedmi/UserController.php
+++ app/Http/Controllers/Gedmi/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Gedmi;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Gedmi\UserModel;
+use App\Models\Gedmi\SiswaModel;
+use Illuminate\Support\Facades\Hash;
 // use Auth;
 
 class UserController extends Controller
@@ -13,12 +15,25 @@ class UserController extends Controller
     {
         return view('content.pages.gedmi.login');
     }
-    
+
     public function apiLogin(Request $request) {
         $username = $request->username;
+        $password = $request->password;
         $loginData = UserModel::where('username', $username)
                                 ->first();
-
-        return $loginData;
+        if($loginData == null) {
+            return response()->json(['pesan' => 'User yang anda masukkan tidak terdaftar']);
+        } else {
+            if(Hash::check($password, $loginData->password)) {
+                if($loginData->role == 'siswa') {
+                    $biodata = SiswaModel::where('id_user', $loginData->id)->first();
+                    $loginData['biodata'] = $biodata;
+                    return response()->json($loginData);
+                }
+                return response()->json($loginData);
+            } else {
+                return response()->json(['pesan' => 'Kata sandi yang anda masukkan salah']);
+            }
+        }
     }
 }
