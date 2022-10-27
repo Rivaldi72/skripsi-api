376cd81c03d464dfe69afaeefdc8cc9d2453a7fb
Rivaldi Aliando <rivdew@gmail.com>
2022-10-27 18:12:12 +0700

    Test

--- app/Http/Controllers/Chairiah/AuthController.php
+++ app/Http/Controllers/Chairiah/AuthController.php
@@ -4,14 +4,28 @@ namespace App\Http\Controllers\Chairiah;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Chairiah\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login() {
         return view('content.pages.chairiah.auth.login');
     }
 
-    public function register(){
+    public function register() {
         return view('content.pages.chairiah.auth.register');
+    }
+
+    public function registerPost(Request $request)
+    {
+        $registerData = new UserModel();
+        $registerData->username = $request->username;
+        $registerData->email = $request->email;
+        $registerData->password = brypt($request->password);
+        $registerData->save();
+        if($registerData) {
+            return redirect()->route('chairiah.login', ['pesan' => 'Berhasil Mendaftar, Silahkan Login Untuk Mengisi Data']);
+        }
     }
 }
