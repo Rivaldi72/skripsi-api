d5918e56497343e1a377a485bb315608846663aa
Cha iriah <chairiah2000@gmail.com>
2022-10-31 13:11:19 +0700

    Optional Value Login

--- resources/views/panels/chairiah/navbar.blade.php
+++ resources/views/panels/chairiah/navbar.blade.php
@@ -64,7 +64,6 @@
                         @if (Auth::check())
                             {{ Auth::user()->name }}
                         @else
-                            {{ dd(session('nama') == '' ? 'Test' : 'Gudang Kopi') }}
                             {{ session('nama') == '' ? session('nama') : 'Gudang Kopi' }}
                         @endif
                     </span>
